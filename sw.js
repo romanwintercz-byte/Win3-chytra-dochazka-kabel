b1fcdac016d557be65bd9262210f63fbc01d9f42
romanwintercz-byte <roman.winter.cz@gmail.com>
2025-12-27 12:27:48 +0100

    start

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// SMARTWORK PWA SERVICE WORKER - v1.8.5
-const CACHE_NAME = 'smartwork-v1.8.5';
+// SMARTWORK PWA SERVICE WORKER - v1.8.6
+const CACHE_NAME = 'smartwork-v1.8.6';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
