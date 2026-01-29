9a9d4b7d302f1ec310431b72fccbf952631e85cb
romanwintercz-byte <roman.winter.cz@gmail.com>
2026-01-29 22:01:39 +0100

    start

--- index.tsx
+++ index.tsx
@@ -3,34 +3,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Signalizace pro index.html
+// Signalizace pro index.html, že React byl načten
 (window as any).APP_STARTED = true;
 
-console.log('Index.tsx: Inicializace Reactu...');
-
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Kritická chyba: Root element #root nebyl nalezen.");
-}
 
-try {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-  
-  // Odstranění loaderu po úspěšném renderu
-  setTimeout(() => {
-    const loader = document.getElementById('app-loader');
-    if (loader) {
-      loader.style.transition = 'opacity 0.4s ease-out';
-      loader.style.opacity = '0';
-      setTimeout(() => loader.remove(), 400);
-    }
-  }, 100);
-
-} catch (err) {
-  console.error('Index.tsx: Selhalo vykreslení aplikace:', err);
+if (rootElement) {
+  try {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+    
+    // Odstranění loaderu
+    setTimeout(() => {
+      const loader = document.getElementById('app-loader');
+      if (loader) {
+        loader.style.opacity = '0';
+        setTimeout(() => loader.remove(), 500);
+      }
+    }, 200);
+  } catch (err) {
+    console.error('Chyba při renderování:', err);
+  }
 }
